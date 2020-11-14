6453c023d6536a066bfe30b0a6851f0e956a5ccd
SentDyem <55694826+SentDyem@users.noreply.github.com>
2020-11-14 21:56:50 +0300

    add test music, import 3d model, fix nativeshare bug

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioMixerGroup audioMixerGroup;
+
+    void Start() {
+        audioSource.outputAudioMixerGroup = audioMixerGroup;
+    }
+}
